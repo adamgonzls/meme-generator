f09f0e6a706635a729a888748000f8a6c30a2d0d
Adam Gonzales <adamgonzales1@gmail.com>
2021-12-31 14:52:45 -0700

    feature: create header, style header, add header content

---
+++ README.md
@@ -0,0 +1,6 @@
+# Meme Generator
+```
+// install dependencies
+npm install
+// npm run dev
+```
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import trollFace from "../img/troll-face.svg"
+
+export default function Header() {
+  return (
+    <header className="app-header">
+      <img className="app-header__icon" src={trollFace} alt="Troll face" />
+      <h1 className="app-header__title">Meme Generator</h1>
+    </header>
+  )
+}
